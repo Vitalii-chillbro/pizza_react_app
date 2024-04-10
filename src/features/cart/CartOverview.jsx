9016c8e54acd6b8e9e07f0f582ee6b59379175aa
Warity <vitaliko1998@gmail.com>
2024-04-10 19:26:56 +0300

    added fix cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -10,7 +10,7 @@ const CartOverview = () => {
     if (!totalCartQuantity) return null
     return (
         <div
-            className="flex items-center justify-between bg-stone-800 px-4 
+            className="fixed bottom-0 left-0 right-0 flex items-center justify-between bg-stone-800 px-4 
         py-4 text-sm
         uppercase text-stone-200 sm:px-6 md:text-base"
         >
